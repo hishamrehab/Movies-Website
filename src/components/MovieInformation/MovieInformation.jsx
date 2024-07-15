2bf4017625d322967455d426a151801d6107ffca
My Name <hishamrehab309@gmail.com>
2024-07-15 03:03:05 +0300

    movie Information page

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
+import { Modal, Typography, Button, ButtonGroup, Grid, Box, CircularProgress, useMediaQuery, Rating } from '@mui/material';
+import { Movie as MovieIcon, Theaters, Language, PlusOne, Favorite, FavoriteBorderOutlined, Remove, ArrowBack } from "@mui/icons-material";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import useStyles from "./styles";
+import { useGetMovieQuery } from "../../services/TMDB";
 
-const Movies = () => {
+
+const MovieInformation = () => {
+    const { id } = useParams();
+    const classes = useStyles();
+    const { data, isFetching, error } = useGetMovieQuery(id);
+
+
+    console.log(data)
+
+    if (isFetching) {
+        return (
+            <Box display="flex" justifyContent="center" alignItems="center">
+                <CircularProgress size="8rem" />
+            </Box>
+        )
+    }
+
+
+    if (error) {
+        return (
+            <Box display="flex" justifyContent="center" alignItems="center">
+                <Link to="/" >Something has done wrong - Go back</Link>
+            </Box>
+        )
+    }
+
 
     return (
-        <h1>
-            MovieInformation
-        </h1>
+        <Grid container className={classes.containerSpaceAround}>
+            <Grid item sm={12} lg={4}>
+                <img className={classes.poster} src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`} alt={data?.title} />
+            </Grid>
+
+            <Grid item container direction="column" lg="7">
+                <Typography variant='h3' align='center' getterBottom>{data?.title} ({data?.release_date.split('-')[0]})</Typography>
+                <Typography variant='h5' align='center' getterBottom>{data?.title} ({data?.tagline}) </Typography>
+                <Typography></Typography>
+            </Grid>
+        </Grid>
     )
 }
 
-export default Movies
+export default MovieInformation
+
---
+++ src/components/MovieInformation/styles.js
@@ -0,0 +1,32 @@
+import { Height } from "@mui/icons-material";
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+  containerSpaceAround: {
+    display: "flex",
+    justifyContent: "space-around",
+    margin: "10px 0 !important",
+    paddingLeft: "220px",
+
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      flexWrap: "wrap",
+      paddingLeft: "0",
+    },
+  },
+  poster: {
+    borderRadius: "20px",
+    boxShadow: "0.5em 1em 1em black rgb(64 , 64 , 70)",
+    width: "80%",
+    [theme.breakpoints.down("sm")]: {
+      margin: "0 auto",
+      width: "100%",
+      height: "350px",
+    },
+    [theme.breakpoints.down("md")]: {
+      margin: "0 auto",
+      width: "50%",
+      height: "350px",
+    },
+  },
+}));
