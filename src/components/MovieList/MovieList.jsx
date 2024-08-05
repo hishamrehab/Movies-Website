ebc9e86d0407a1979077c6debd267060e91ac896
My Name <hishamrehab309@gmail.com>
2024-08-05 18:03:21 +0300

    Finish the proejct and deploy on firebase

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,13 +3,14 @@ import { Grid } from '@mui/material';
 import useStyles from "./styles";
 import Movie from '../Movie/Movie';
 
-export const MovieList = ({ movies, i, numberOfMovies }) => {
+export const MovieList = ({ movies, i, numberOfMovies, excludeFirst }) => {
     const classes = useStyles();
+    const startFrom = excludeFirst ? 1 : 0;
 
     console.log(movies);
     return (
         <Grid container className={classes.moviesContainer}>
-            {movies.results.slice(0, numberOfMovies).map((movie) => (
+            {movies.results.slice(startFrom, numberOfMovies).map((movie) => (
                 <Movie key={i} movie={movie} i={i} />
             ))}
         </Grid>
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
 import { MovieList } from '../MovieList/MovieList';
-import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
 import Pagination from '../Pagination/Pagination';
 import useStyles from "./styles";
+import FeaturedMovie from '../FeaturedMovie/FeaturedMovie';
 const Movies = () => {
     const [page, setPage] = useState(1);
     const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
@@ -13,7 +13,7 @@ const Movies = () => {
     const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
     const classes = useStyles();
 
-    const numberOfMovies = lg ? 16 : 18
+    const numberOfMovies = lg ? 17 : 19
 
     if (isFetching) {
         return (
@@ -38,7 +38,8 @@ const Movies = () => {
     if (error) return 'An error has occured.';
     return (
         <div className={classes.moviesContainer}>
-            <MovieList movies={data} numberOfMovies={numberOfMovies} />
+            <FeaturedMovie movie={data.results[0]} />
+            <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
             <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
         </div>
     )
